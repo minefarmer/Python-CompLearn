b321fbd783d9a524394020a558922c19827c52da
minefarmer <pgoldfarm@yahoo.com>
2022-06-11 16:50:52 -0500

    cleaning up

---
+++ my_notes/python_modules_packeges/built-in_modules.py
@@ -0,0 +1,23 @@
+'''             BUILT-IN MODULES
+These are modules Python has already creates.
+I cna use them by importing.
+
+
+
+
+
+
+'''
+import random
+import platform
+
+x = random.random()
+print(x)  # 0.18881411874704246
+
+y = random.randint(0,50)
+print(y)  # 10
+
+j = platform.system()
+print(j)  # Linux
+
+
---
+++ my_notes/python_modules_packeges/my_packeges.py
@@ -0,0 +1,12 @@
+'''             WHAT ARE PACKEGES?
+Packages are just folders(directories) that contains modules.
+They contain a special python file named:__init__.py
+The __init__.py can be empty. The file tells Python that the directory or folder contains a python package which can be imported like a module.
+Packages are a convient way to organize modules.
+A package can contain sub modules.
+
+            CREATING A PACKAGE
+Creating a package is fairly straightforward if you're going to do it manually, you create a directory, inside that directory, you create a python file and name it underscore, underscore init underscore. underscore.py
+'''
+
+
