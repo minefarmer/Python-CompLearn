14e835ddfe6813d164f18d13b2f75c93f14b09ff
minefarmer <pgoldfarm@yahoo.com>
2022-06-18 17:11:27 -0500

    linux here i come!

---
+++ my_notes/images/image_manipulation.py
@@ -0,0 +1,27 @@
+'''             IMAGE MANIPULATION
+Convert an image to grayscale.
+Convert an image to another data type.
+Resize an image.
+Rotate an image.
+
+
+
+
+'''
+# load and show an image with Pillow
+from turtle import clear
+import pillow
+
+# Load the image
+img = Image.open('pup_images/tan.jpeg') .convert('L')
+
+# Get basic details about the image
+print(img.format)  # JPEG
+print(img.mode)  # RGB
+print(img.size)  # (290, 193)
+
+# Show image
+img.show()
+
+#  show image
+img.save("puppies/tan.jpeg")
